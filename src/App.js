5bc39787511c8bf625231872c5d067fb810886ec
husain <husain68644@gmail.com>
2023-02-15 11:53:09 +0530

    no change

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Main from "./Home/main";
+
+function App() {
+  return (
+    <div>
+    <Main />
+    </div>
+  );
+  }
+
+export default App;
+
+
+
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+function Header() {
+  return (
+    <div className="main">
+        <text>hello</text>
+    </div>
+  )
+}
+
+export default Header
